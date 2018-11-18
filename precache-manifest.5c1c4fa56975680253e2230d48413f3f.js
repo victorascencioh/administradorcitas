1035b2c03c7f06874dfc043390c610500a3f8060
victorascencioh <victorascencioh@gmail.com>
2018-11-18 19:44:21 +0100

    Updates

---
+++ precache-manifest.5c1c4fa56975680253e2230d48413f3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fed1f293b2b91f685c2",
+    "url": "/administradorcitas/static/js/runtime~main.3fed1f29.js"
+  },
+  {
+    "revision": "d00128cfef36ffa7256f",
+    "url": "/administradorcitas/static/js/main.d00128cf.chunk.js"
+  },
+  {
+    "revision": "795591b815ad9a3c4656",
+    "url": "/administradorcitas/static/js/1.795591b8.chunk.js"
+  },
+  {
+    "revision": "d00128cfef36ffa7256f",
+    "url": "/administradorcitas/static/css/main.0c7a2858.chunk.css"
+  },
+  {
+    "revision": "51a440680eb23184f879b664b874db0b",
+    "url": "/administradorcitas/index.html"
+  }
+];
